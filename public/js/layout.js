473c9cac44967bf067744c01e4cdcd54b17b65a9
huy14032003 <trhuy14032003@gmail.com>
2025-08-24 01:55:27 +0700

    input

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,57 +1,158 @@
-const item = document.querySelector(".sidebar .item");
-const menuIcon = document.querySelector(".menu-icon");
-const allCollapse = document.querySelectorAll(".collapse");
-const allArrow = document.querySelectorAll(".bi-chevron-right");
-const Wrapper = document.querySelector(".wrapper-container");
-const toggle = document.querySelectorAll(".sidebar .link .item .toggle");
-const iconArrow = document.querySelectorAll(
-  ".sidebar .link .item .bi-chevron-right"
-);
-const itemCollapse = document.querySelectorAll(".collapse");
-const profile=document.querySelector('.profile');
-const multiLang=document.querySelector('.multi-lang');
-const sidebar=document.querySelector('.sidebar')
-
-let isClick=false;
-menuIcon.addEventListener("click", () => {
-  Wrapper.classList.toggle("open");
-  Wrapper.classList.toggle("collsapsed");
-  isClick=!isClick
-  
-  // Đóng tất cả các menu con
-  allCollapse.forEach((collapse) => {
-    collapse.classList.remove("show");
-  });
-
-  // Reset mũi tên
-  allArrow.forEach((arrow) => {
-    arrow.classList.remove("rotate");
-  });
-});
-sidebar.addEventListener("mouseenter", () => {
-    if (!isClick) {
-    Wrapper.classList.remove("collsapsed");
+const Sidebar=(()=>{
+  const _opts={
+    menuIcon:null,
+    sideBar:null,
+    navBar:null,
+    app_main:null,
+    btnDarkMode:null,
+    groupMultiLang:null,
+    multiBox:null,
   }
-});
+  const ui={
+    get menuIcon()
+    {
+      return document.querySelector(_opts.menuIcon)
+    },
+    get sideBar()
+    {
+      return document.querySelector(_opts.sideBar)
+    },
+    get navBar()
+    {
+      return document.querySelector(_opts.navBar)
+    },
+    get app_main()
+    {
+      return document.querySelector(_opts.app_main)
+    },
+    get btnDarkMode()
+    {
+      return document.querySelector(_opts.btnDarkMode)
+    },
+    get groupMultiLang()
+    {
+      return document.querySelector(_opts.groupMultiLang)
+    },
+    get multiBox()
+    {
+      return document.querySelector(_opts.multiBox)
+    },
+  };
+  const state={
+    isDark:false,
+  };
 
-sidebar.addEventListener("mouseleave", () => {
-  if (!isClick) {
-    Wrapper.classList.add("collsapsed");
+
+function handleEventListener()
+{
+handleClickMenu()
+handleClickDarkMode()
+handleClickMulti();
+}
+
+
+function handleClickMenu()
+{
+  ui.menuIcon.addEventListener('click',()=>{
+    ui.sideBar.classList.toggle("collapsed")
+    ui.navBar.classList.toggle("collapsed")
+    ui.app_main.classList.toggle("collapsed")
+  })
+}
+function handleClickDarkMode()
+{
+  ui.btnDarkMode.addEventListener('click',()=>{
+    state.isDark = !state.isDark;
+    console.log(state.isDark);
+    ui.btnDarkMode.classList.toggle('darkMode');
+    document.body.classList.toggle("darkMode");
+    state.isDark? ui.btnDarkMode.innerHTML=`<i class="bi bi-brightness-high fs-5 text-light"></i>`:ui.btnDarkMode.innerHTML=` <i class="bi bi-moon-stars fs-5"></i>`
+  })
+}
+function handleClickMulti()
+{
+  ui.groupMultiLang.addEventListener('click',()=>{
+    ui.multiBox.classList.toggle('d-none')
+  })
+}
+  function init(opt = {})
+  {
+    Utils.merge(_opts,opt)
+    handleEventListener()
   }
-}); 
-let isProcessing = false;
+  return{init};
+})();
 
-toggle.forEach((item, index) => {
-  item.addEventListener("click", (e) => {
-    if (isProcessing) {
-      e.preventDefault(); // chặn click liên tục
-      return;
+const Utils = (() => {
+
+    /**
+     * @param {object} target The object that is store data
+     * @param {object} resorce The object that need passing data to target
+     * @returns {object} The object is merged
+     */
+    function merge (target, resorce) {
+        for (let i in resorce) {
+            if (
+                i in target &&
+                typeof resorce[i] === 'object' &&
+                typeof target[i] === 'object'
+            ) {
+                merge(target[i], resorce[i]);
+                continue;
+            }
+            target[i] = resorce[i];
+        }
+        return target;
+    };
+
+    function debounce (func, timeout = 300) {
+        let timer;
+        return (...args) => {
+            clearTimeout(timer);
+            timer = setTimeout(() => { func.apply(this, args) }, timeout);
+        };
     }
-    isProcessing = true;
-    setTimeout(() => {
-      isProcessing = false;
-    }, 350);
-    Wrapper.classList.remove("collsapsed");
-    iconArrow[index].classList.toggle("rotate");
-  });
-});
+
+    function throttle (func, timeout = 300) {
+        let lastTime = 0;
+        return (...args) => {
+            let now = Date.now();
+            if (now - lastTime >= timeout) {
+                func.apply(this, args);
+                lastTime = now;
+            }
+        }
+    }
+
+    function getParamsURL (str) {
+        const prm = decodeURIComponent(decodeURIComponent(new URLSearchParams(location.search).get(str)));
+        return prm === 'null' ? null : prm;
+    }
+
+    /**
+     * @param {string} field The name of params that you want to change the value.
+     * @param {string | number} value The value must be change.
+     */
+    function changeParamsURL (field, value) {
+        const url = new URL(window.location);
+        url.searchParams.set(field, value);
+        const newUrl = url.toString().replace(/\+/g, '%20');
+        window.history.pushState({}, '', newUrl);
+    }
+
+    /**
+     * 
+     * @param {any} val Giá trị bất kỳ
+     */
+    function fixNullish(val) {
+        return val ?? 'N/A';
+    }
+    return {
+        debounce,
+        throttle,
+        getParamsURL,
+        changeParamsURL,
+        merge,
+        fixNullish
+    }
+})();
